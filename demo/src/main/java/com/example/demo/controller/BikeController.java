61a5ea9844bf588406bbbae477c543069e827cfe
Abby <wzr314@me.com>
2019-07-04 13:36:59 +0800

    connected

--- demo/src/main/java/com/example/demo/controller/BikeController.java
+++ demo/src/main/java/com/example/demo/controller/BikeController.java
@@ -1,16 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;	//Json dada
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.pojo.Bike;
 
 
 @Controller
 public class BikeController {
-
+	
+	//test
 	@RequestMapping("/hi")
 	@ResponseBody
 	public String hello(String mgs) {
 		return "hello " + mgs;
+	}
+	
+	//add bike data to server side
+	@RequestMapping("/addBike")
+	@ResponseBody
+	public String add(@RequestBody Bike bike) {
+		System.out.print(bike);
+		
+		return "end";
 	}
 }
